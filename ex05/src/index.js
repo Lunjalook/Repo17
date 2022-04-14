28788e9df448911e2de6b678d54eba158ed89f9f
Lunjalook <Azra@admins-MacBook-Air.local>
2022-04-14 06:46:53 +0200

    Adding exercise 05-Destructuring Arrays

--- ex05/src/index.js
+++ ex05/src/index.js
@@ -3,8 +3,9 @@ let foo = 12, boo = 45;
 function main(foo, boo) {
     // Only change code below this line
     [foo, boo] = [boo, foo];
+    return { foo, boo };
     // Only change code above this line
-    return { foo, boo }
+
 }
 
 console.log(main(foo, boo));
